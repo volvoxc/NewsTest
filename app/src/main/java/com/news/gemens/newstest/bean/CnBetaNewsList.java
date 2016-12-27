93e1b8dca9249e1efc8fbb61b9d31c4938071e0d
lvyunjiao <lvyunjiao@wiseweb.com.cn>
2016-12-27 09:34:56 +0800

    框架搭建

---
+++ app/src/main/java/com/news/gemens/newstest/bean/CnBetaNewsList.java
@@ -0,0 +1,10 @@
+package com.news.gemens.newstest.bean;
+
+/**
+ * Created by Gemens on 2016/12/26/0026.
+ */
+
+public class CnBetaNewsList {
+
+
+}
---
+++ app/src/main/java/com/news/gemens/newstest/news/presenter/NewsPresenter.java
@@ -0,0 +1,9 @@
+package com.news.gemens.newstest.news.presenter;
+
+/**
+ * Created by Gemens on 2016/12/26/0026.
+ */
+
+public class NewsPresenter {
+
+}
