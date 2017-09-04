eca83dabf065962417097edcc800f920c99accd2
lvyunjiao <gemenslv@gmail.com>
2017-09-04 17:05:36 +0800

    add hahah

--- app/src/main/java/com/news/gemens/newstest/main/MainActivity.java
+++ app/src/main/java/com/news/gemens/newstest/main/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
 
         switchToNews();
+        "hahahhahaah"
     }
 
     private void init() {
