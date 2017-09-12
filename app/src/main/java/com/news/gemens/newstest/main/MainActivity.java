e2e367bf75acee6074deff11bd2c97d9083f9158
lvyunjiao <gemenslv@gmail.com>
2017-09-12 16:53:02 +0800

    commit

--- app/src/main/java/com/news/gemens/newstest/main/MainActivity.java
+++ app/src/main/java/com/news/gemens/newstest/main/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
 
         switchToNews();
+        //hahhahahahha
     }
 
     private void init() {
